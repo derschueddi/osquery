8e95a2cae2207b65e89a7ac0f574b93bc67fb1a7
Steffen Haas <haas@informatik.uni-hamburg.de>
2018-05-31 01:21:58 +0200

    Revert "minor broker updates: raspi support among others"
    This reverts commit f10c8f535ab62659f1299a8e8f4fbc4632388dde.

--- tools/provision/formula/caf.rb
+++ tools/provision/formula/caf.rb
@@ -4,10 +4,9 @@ class Caf < AbstractOsqueryFormula
   desc "Implementation of the Actor Model for C++"
   homepage "https://actor-framework.org/"
   url "https://github.com/actor-framework/actor-framework.git",
-      :revision => "882ba63d1775d8f8a5c1c514decd610134988d8c"
+      :revision => "68a568677cd8d86aaeac1e1b98bfc87a39e5be34"
   head "https://github.com/actor-framework/actor-framework.git"
-  version "0.15.7"
-  revision 1
+  version "0.15.5"
 
   needs :cxx11
 
@@ -37,7 +36,7 @@ diff --git a/CMakeLists.txt b/CMakeLists.txt
 index b56c979..7aaf516 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
-@@ -342,9 +342,9 @@ if(CAF_IOS_DEPLOYMENT_TARGET)
+@@ -334,9 +334,9 @@ if(CAF_IOS_DEPLOYMENT_TARGET)
    endif()
  endif()
  # check if the user provided CXXFLAGS, set defaults otherwise
@@ -54,7 +53,7 @@ diff --git a/libcaf_core/caf/intrusive_ptr.hpp b/libcaf_core/caf/intrusive_ptr.h
 index 25d8a01..580dca3 100644
 --- a/libcaf_core/caf/intrusive_ptr.hpp
 +++ b/libcaf_core/caf/intrusive_ptr.hpp
-@@ -247,7 +247,7 @@ std::string to_string(const intrusive_ptr<T>& x) {
+@@ -249,7 +249,7 @@ std::string to_string(const intrusive_ptr<T>& x) {
    // we convert to hex representation, i.e.,
    // one byte takes two characters + null terminator + "0x" prefix
    char buf[sizeof(v) * 2 + 3];
