cd2aed3d06f0a5132b62959862dbfe9a5d70be53
Steffen Haas <haas@informatik.uni-hamburg.de>
2018-08-16 15:36:17 +0200

    Bro Integration

---
+++ tools/provision/formula/caf.rb
@@ -0,0 +1,52 @@
+require File.expand_path("../Abstract/abstract-osquery-formula", __FILE__)
+
+class Caf < AbstractOsqueryFormula
+  desc "Implementation of the Actor Model for C++"
+  homepage "https://actor-framework.org/"
+  url "https://github.com/actor-framework/actor-framework.git",
+      :revision => "656d0bd236f95514b855e4012251ddb270468f78"
+  head "https://github.com/actor-framework/actor-framework.git"
+  version "0.15.7"
+  revision 3
+
+  needs :cxx11
+
+  bottle do
+    root_url "https://osquery-packages.s3.amazonaws.com/bottles"
+    cellar :any_skip_relocation
+  end
+
+  depends_on "openssl"
+  depends_on "cmake" => :build
+
+  # Use both provided and default CXX flags
+  patch :DATA
+
+  def install
+    args = %W[--prefix=#{prefix} --no-examples --no-unit-tests --no-opencl --no-benchmarks --no-tools --no-python --build-static-only]
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+  end
+
+end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 9402137..d4268c4 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -336,9 +336,9 @@ if(CAF_IOS_DEPLOYMENT_TARGET)
+   endif()
+ endif()
+ # check if the user provided CXXFLAGS, set defaults otherwise
+-if(NOT CMAKE_CXX_FLAGS)
+-  set(CMAKE_CXX_FLAGS                   "-std=c++11 -Wextra -Wall -pedantic ${EXTRA_FLAGS}")
+-endif()
++#if(NOT CMAKE_CXX_FLAGS)
++set(CMAKE_CXX_FLAGS                   "${CMAKE_CXX_FLAGS} -std=c++11 -Wextra -Wall -pedantic ${EXTRA_FLAGS}")
++#endif()
+ if (NOT "${CMAKE_CXX_FLAGS}" MATCHES "-std=")
+   message(STATUS "Supplied CXXFLAGS do not contain a C++ standard, setting std to c++11")
+   set(CMAKE_CXX_FLAGS                   "-std=c++11 ${CMAKE_CXX_FLAGS}")
